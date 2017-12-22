93626651e9fc7ce2df8daf727af95988f93dd3ec
l-wong <lisa.c.wong@ryerson.ca>
2017-12-22 16:30:33 -0500

    R2

---
+++ R2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class R2 {
+	
+	public static void main(String[] args){
+		
+		Scanner scanner = new Scanner(System.in);
+		int r1 = scanner.nextInt();
+		int s = scanner.nextInt();
+		
+		int r2 = (s * 2) - r1;
+		
+		System.out.println(r2);
+		
+	}
+
+}
